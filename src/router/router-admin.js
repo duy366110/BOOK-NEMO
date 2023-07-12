86fc602b12557280fa60f542541605fd8343f282
duy366110 <duy366110@gmail.com>
2023-07-12 18:13:43 +0700

    #Commit 12/07/2023: Config render page user - role.

--- src/router/router-admin.js
+++ src/router/router-admin.js
@@ -2,8 +2,11 @@ const router = require('express').Router();
 const ControllerAdmin = require("../controller/controller-admin");
 
 router.get("/", ControllerAdmin.renderPageAdmin);
+router.get('/user', ControllerAdmin.renderPageAdminUser);
+router.get('/role', ControllerAdmin.renderPageAdminRole);
 router.get('/product/edit', ControllerAdmin.renderPageEditProduct);
 router.get('/product/new', ControllerAdmin.renderPageNewProduct);
+
 router.post('/product/new', ControllerAdmin.saveProduct);
 router.post('/product/edit', ControllerAdmin.editProduct);
 
