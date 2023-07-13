ce91a223696a823692463a37278786fe4e81f490
duy366110 <duy366110@gmail.com>
2023-07-13 15:39:40 +0700

    #Commit 13/07/2023: Config csurf - validation form.

--- src/router/router-user.js
+++ src/router/router-user.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 const MiddlewareUser = require("../middleware/middleware-user");
 const ControllerUser = require("../controller/controller-user");
 
@@ -7,7 +7,23 @@ router.get('/signin', ControllerUser.renderUserSignin);
 router.get("/signup", ControllerUser.renderUserSignup);
 
 router.get('/signout', ControllerUser.userSignout);
-router.post("/signin", [check('email').isEmail().withMessage('Invalid e-mail')], ControllerUser.userSignin);
+
+router.post("/signin",
+    [
+        check('email').isEmail().withMessage('Please enter e-mail'),
+        body('password').trim().custom((value, {req}) => {
+            if(!value) {
+                throw new Error('Password cannot be empty');
+            }
+
+            if(value.length < 5 || value.length > 20) {
+                throw new Error('Password must be longer than 5 characters and less than 20 characters');
+            }
+            return true;
+        })
+    ],
+    ControllerUser.userSignin);
+
 router.post('/signup', MiddlewareUser.userExists, ControllerUser.userSignup);
 
 module.exports = router;
