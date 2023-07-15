15eb067bcf6f7fad6a035636eb5f14b35a8b96b9
duy366110 <duy366110@gmail.com>
2023-07-15 22:56:54 +0700

    #Commit 15/07/2023: Config create role.

--- src/router/router-role.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router();
-const ControllerRole = require("../controller/controller-role");
-
-
-module.exports = router;
--- src/router/router.js
+++ src/router/router.js
@@ -4,7 +4,6 @@ const RouterAdmin = require("./router-admin");
 const RouterUser = require("./router-user");
 const RouterProduct = require("./router-product");
 const RouterOrder = require("./router-order");
-const RouterRole = require("./router-role");
 const ControllerException = require("../controller/controller-exception");
 
 router.use('/', RouterHome);
@@ -12,7 +11,6 @@ router.use('/admin', RouterAdmin);
 router.use('/user', RouterUser);
 router.use('/product', RouterProduct);
 router.use("/order", RouterOrder);
-router.use("/role", RouterRole);
 
 router.use(ControllerException.renderinternalServerFailed);
 router.use(ControllerException.renderNotFoundPage);
