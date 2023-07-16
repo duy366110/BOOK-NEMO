d95fb671cec62a7bab6607ebe6926d830140ae88
duy366110 <duy366110@gmail.com>
2023-07-16 22:13:52 +0700

    #Commit 16/07/2023: Config done responsive tab navigator.

--- src/public/js/common/header.js
+++ src/public/js/common/header.js
@@ -1,10 +1,18 @@
 window.onload = function(event) {
     const $ = document.querySelector.bind(document);
     let hamburger = $('#header-hamburger');
+    let navigationtab = $('#navigation-tab');
+    let tabmask = $('#tab-mask');
 
     console.log(hamburger);
 
     hamburger.addEventListener('click', (event) => {
         hamburger.classList.toggle('active');
+        navigationtab.classList.add('active');
+    })
+
+    tabmask.addEventListener('click', (event) => {
+        hamburger.classList.remove('active');
+        navigationtab.classList.remove('active');
     })
 }
