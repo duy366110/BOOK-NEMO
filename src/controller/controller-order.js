6c8c625cf27d5f9003e5bda6af103e83e4219504
duy366110 <duy366110@gmail.com>
2023-08-05 15:40:59 +0700

    #Commit 05/08/2023: Config download file pdf.

--- src/controller/controller-order.js
+++ src/controller/controller-order.js
@@ -2,6 +2,7 @@ const mongodb = require("mongodb");
 const ModelOrder = require("../model/model-order");
 const ModelUser = require("../model/model-user");
 const pdf = require('pdf-creator-node');
+const htmlToPdf = require("html-pdf-node");
 const path = require('path');
 const fs = require('fs');
 const ObjectId = mongodb.ObjectId;
@@ -79,75 +80,105 @@ class ControllerOrder {
                             .exec();
 
             if(orderInfor) {
-                let information = {
-                    user_name: orderInfor.user_id.name,
-                    user_email: orderInfor.email,
-                    total: Number(orderInfor.order.reduce((acc, order) => {
-                        acc += parseFloat(order.quantity) * parseFloat(order.product.price);
-                        return acc;
-                    }, 0)).toFixed(3),
-                    orders: orderInfor.order.map((order, index) => {
-                        return {
-                            index,
-                            product_name: order.product.title,
-                            product_image: order.product.image,
-                            product_price: order.product.price,
-                            quantity: order.quantity,
-                        }
-                    })
-                }
 
-                let html = fs.readFileSync(path.join(__dirname, "../", "document", "invoice.html"), "utf-8");
-                let options = {
-                    format: "A4",
-                    orientation: "portrait",
-                    border: "10mm",
-                    localUrlAccess: true,
-                    header: {
-                        height: "15mm",
-                        contents: '<div style="text-align: center;"></div>'
-                    },
-                    footer: {
-                        height: "30.7mm",
-                        contents: {
-                            first: 'Cover page',
-                            2: 'Second page', // Any page number is working. 1-based index
-                            default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
-                            last: 'Last Page'
-                        }
-                    }
-                };
+                let pathFile = path.join(__dirname, "../", "document", "invoice.pdf");
+                let filePDF = fs.createReadStream(pathFile);
 
-                var document = {
-                    html: html,
-                    data: {
-                    bill: information,
-                    },
-                    path: path.join(__dirname, "../", "document", "invoice.pdf"),
-                };
+                res.setHeader("Content-Type", "application/pdf");
+                res.setHeader('Content-Disposition', "inline; filename=invoice.pdf");
+                filePDF.pipe(res);
 
-                await pdf.create(document, options, {
-                    childProcessOptions: {
-                      env: {
-                        OPENSSL_CONF: '/dev/null',
-                      },
-                    }
-                  });
+
+
+                // let options = { format: 'A4' };
+                // let file = [{
+                //     url: path.join(__dirname, "../", "document", "invoice.html"),
+                //     name: 'invoice.pdf',
+                //     path: path.join(__dirname, "../", "document")
+                // }];
+                
+                // htmlToPdf.generatePdfs(file, options).then(pdfBuffer => {
+                //     console.log("PDF Buffer:-", pdfBuffer);
+
+                //     // console.log(Buffer.concat(pdfBuffer));
+                //     // let doc = fs.createReadStream(Buffer.concat(pdfBuffer));
+
+                //     // res.setHeader('Content-Type', 'application/pdf');
+                //     // res.setHeader('Content-Disposition',"inline; filename=invoice.pdf");
+
+                //     // fileDoc.pipe(res);
+                //     // res.send(pdfBuffer);
+                // });
+
+                // let information = {
+                //     user_name: orderInfor.user_id.name,
+                //     user_email: orderInfor.email,
+                //     total: Number(orderInfor.order.reduce((acc, order) => {
+                //         acc += parseFloat(order.quantity) * parseFloat(order.product.price);
+                //         return acc;
+                //     }, 0)).toFixed(3),
+                //     orders: orderInfor.order.map((order, index) => {
+                //         return {
+                //             index,
+                //             product_name: order.product.title,
+                //             product_image: order.product.image,
+                //             product_price: order.product.price,
+                //             quantity: order.quantity,
+                //         }
+                //     })
+                // }
+
+                // let html = fs.readFileSync(path.join(__dirname, "../", "document", "invoice.html"), "utf-8");
+                // let options = {
+                //     format: "A4",
+                //     orientation: "portrait",
+                //     border: "10mm",
+                //     localUrlAccess: true,
+                //     header: {
+                //         height: "15mm",
+                //         contents: '<div style="text-align: center;"></div>'
+                //     },
+                //     footer: {
+                //         height: "30.7mm",
+                //         contents: {
+                //             first: 'Cover page',
+                //             2: 'Second page', // Any page number is working. 1-based index
+                //             default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+                //             last: 'Last Page'
+                //         }
+                //     }
+                // };
+
+                // var document = {
+                //     html: html,
+                //     data: {
+                //     bill: information,
+                //     },
+                //     path: path.join(__dirname, "../", "document", "invoice.pdf"),
+                // };
+
+                // await pdf.create(document, options, {
+                //     childProcessOptions: {
+                //       env: {
+                //         OPENSSL_CONF: '/dev/null',
+                //       },
+                //     }
+                //   });
 
                 // pdf.create(html, { childProcessOptions: { env: { OPENSSL_CONF: '/dev/null' }}).toFile(`./public/invoices/${order.id}.pdf`, (err, res) => {
                 //     if (err) return console.log(err);
                 // });
 
-                let pdfPath = path.join(__dirname, "../", "document", "invoice.pdf");
+                // let pdfPath = path.join(__dirname, "../", "document", "invoice.pdf");
                 // let fileDoc = fs.createReadStream(pdfPath);
 
-                res.setHeader('Content-Type', 'application/pdf');
-                res.setHeader('Content-Disposition',"inline; filename=invoice.pdf");
+                // res.setHeader('Content-Type', 'application/pdf');
+                // res.setHeader('Content-Disposition',"inline; filename=invoice.pdf");
 
                 // fileDoc.pipe(res);
 
-                let data = fs.readFileSync(pdfPath);
-                res.send(data);
+                // let data = fs.readFileSync(pdfPath);
+                // res.send(data);
 
 
             } else {
