3a7da087b3ddf614975148f658288d7d13367b7d
duy366110 <duy366110@gmail.com>
2023-08-07 18:13:38 +0700

    #Commit 07/08/2023: Config render product in product admin.

--- src/model/model-product.js
+++ src/model/model-product.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mongodb = require("mongodb");
 const Schema = mongoose.Schema;
 
 const ModelProduct = new Schema({
@@ -11,12 +12,16 @@ const ModelProduct = new Schema({
         default: ''
     },
     price: {
-        type: Number,
+        type: mongoose.Schema.Types.Decimal128,
         default: 0
     },
     description: {
         type: String,
         default: ''
+    },
+    product_ref: {
+        type: Number,
+        default: 0
     },
     status: {
         type: Boolean,
