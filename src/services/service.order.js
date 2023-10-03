5f763a7f535bac8e13c1fe02e301b590be6188cf
duy366110 <duy366110@gmail.com>
2023-10-03 13:14:13 +0700

    #Commit 03/10/2023: Config refactor order - payment.

--- src/services/service.order.js
+++ src/services/service.order.js
@@ -1,5 +1,7 @@
 "use strict"
-const ModelUser = require("../model/model-order");
+const pdfkitTable = require("pdfkit-table");
+const path = require('path');
+const fs = require("fs");
 const ModelOrder = require("../model/model-order");
 
 class ServiceOrder {
@@ -26,7 +28,6 @@ class ServiceOrder {
     // ORDER
     async order(user = {}, cb) {
         try {
-
             if(user.model.order) {
                 user.model.cart = user.model.cart.map( async(cartElm) => {
 
@@ -39,12 +40,11 @@ class ServiceOrder {
                             return colElm;
                         })
 
-                        await user.model.order.save();
-
                     } else {
                         user.model.order.collections.push(cartElm);
                     }
 
+                    await user.model.order.save();
                     return null;
                 })
 
@@ -63,6 +63,104 @@ class ServiceOrder {
 
             await user.model.save();
             cb({status: true, message: "User order product successfully"});
+
+        } catch (error) {
+            // METHOD FAILED
+            cb({status: false, message: 'Method failed', error});
+        }
+    }
+
+    // CANCEL ORDER
+    async cancel(user = {}, cb) {
+        try {
+            user.model.order.collections.map(async (colElm) => {
+                colElm.product.quantity += colElm.quantity;
+
+                if(colElm.product.ref) {
+                    colElm.product.ref--;
+                }
+
+                await colElm.product.save();
+
+                return colElm;
+            })
+
+            await user.model.order.deleteOne();
+            user.model.order = null;
+            await user.model.save();
+            cb({status: true, message: 'Cancel user order successfully'});
+
+        } catch (error) {
+            // METHOD FAILED
+            cb({status: false, message: 'Method failed', error});
+        }
+    }
+
+    // CREATE INVOICE
+    async invoice(user = {}, res = {}, cb) {
+        try {
+            if(user.model) {
+                const doc = new pdfkitTable({ margin: 30, size: 'A4', encodeURI: 'UTF-8' });
+
+                let pathFont = path.join(__dirname,'../', 'public', 'fonts', 'Roboto-Regular.ttf' )
+
+                doc.font(pathFont);
+                doc.pipe(fs.createWriteStream(path.join(__dirname, "../", "document", 'invoice.pdf')));
+                doc.pipe(res);
+
+                doc.fontSize(18).text('HOÁ ĐƠN KHÁCH HÀNG', {width: 535, align: 'center'});
+
+                let total = Number(
+                    user.model.order.collections.reduce((acc, order) => {
+                        acc += parseFloat(order.product.price) * Number(order.quantity);
+                        return acc;
+                    }, 0)
+                ).toFixed(3);
+
+                let rowsInfor = [
+                    ['Họ và tên', `${user.model.name}`],
+                    ['E-mail', `${user.model.email}`],
+                    ['Tổng hoá đơn', `${total} VND`]
+                ]
+
+                let rowsProduct = [
+                   ...user.model.order.collections.map((order, index) => {
+                    return [
+                        `${index}`,
+                        `${order.product.title}`,
+                        `${Number(order.product.price).toFixed(3)} VND`,
+                        `${order.quantity}`,
+                    ]
+                   })
+                ]
+
+                const table = {
+                    title: "Đơn hàng: sách",
+                    subtitle: "Trạng thái: thanh toán hoá đơn sau",
+                    headers: [ "STT", "Tên sản phẩm", "Giá", "Số Lượng" ],
+                    rows: [
+                      ...rowsProduct,
+                      ...rowsInfor
+                    ],
+                };
+
+                doc.table(table, {
+                    width: 535,
+                    prepareHeader: () => {
+                        doc.font(pathFont)
+                    },
+                    prepareRow: (row, indexColumn, indexRow, rectRow) => {
+                        doc.font(pathFont);
+                        indexColumn === 0 && doc.addBackground(rectRow, 'blue', 0.15);
+                    },
+                });
+
+                doc.end();
+                cb({status: true, message: 'Create voice successfully'});
+
+            } else {
+                cb({status: false, message: 'Create voice unsuccessfully'});
+            }
 
         } catch (error) {
             // METHOD FAILED
---
+++ src/services/service.payment.js
@@ -0,0 +1,69 @@
+"use strict"
+const CONFIG_PAYPAL = require("../configs/config.paypal");
+const paypal = require('paypal-rest-sdk');
+
+class ServicePayment {
+
+    constructor() { }
+
+    async payment(user = {}, cb) {
+        try {
+            // TÍNH TỔNG HOÁ ĐƠN
+            let total = Number(user.model.order.collections.reduce((acc, order) => {
+                acc += Number(order.product.price) * Number(order.quantity);
+                return acc;
+            }, 0)).toFixed(2);
+
+            // CẤU HÌNH PAYPAL THANH TOÁN HOÁ ĐƠN
+            let create_payment_json = {
+                "intent": "sale",
+                "payer": {
+                    "payment_method": "paypal"
+                },
+                "redirect_urls": {
+                    "return_url": CONFIG_PAYPAL.PAGE_SUCCESS,
+                    "cancel_url": CONFIG_PAYPAL.PAGE_CANCEL
+                },
+                "transactions": [{
+                    "item_list": {
+                        "items": [{
+                            "name": "Payment order",
+                            "sku": "Payment order",
+                            "price": parseFloat(total),
+                            "currency": "USD",
+                            "quantity": 1
+                        }]
+                    },
+                    "amount": {
+                        "currency": "USD",
+                        "total": parseFloat(total)
+                    },
+                    "description": "This is the payment description."
+                }]
+            };
+
+            // TIẾN HÀNH THANH TOÁN
+            paypal.payment.create(create_payment_json, function (error, payment) {
+
+                if (error) {
+                    throw error;
+
+                } else {
+                    // PAYPAL CHẤP NHẬN THANH TOÁN - CHUYỂN ĐẾN TRANG THANH TOÁN
+                    for(let link of payment.links ) {
+                        if(link.rel === 'approval_url') {
+                            cb({status: true, message: 'User payment order product successfully', link: link.href});
+                            return;
+                        }
+                    }
+                }
+            });
+
+        } catch (error) {
+            // METHOD FAILED
+            cb({status: false, message: 'Method failed', error});
+        }
+    }
+}
+
+module.exports = new ServicePayment();
